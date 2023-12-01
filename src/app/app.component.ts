6e1f48f03577e30a85c2903d475b69fea434cd8c
PabloNavarroUrbano <panaur@floridauniversitaria.es>
2023-12-01 09:33:44 +0100

    subiendo nuevo componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
