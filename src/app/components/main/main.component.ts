caaf137f5445047992bd25888c067c9960962218
PabloNavarroUrbano <panaur@floridauniversitaria.es>
2023-12-01 11:32:28 +0100

    subiendo article

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+import { ArticleComponent } from '../article/article.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, ArticleComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
